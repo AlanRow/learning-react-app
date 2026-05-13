9035fa0d52255e36c9384d8c41dd169c32153d5f
Student <Student@top.ru>
2026-05-13 19:29:16 +0300

    Conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -4,112 +4,47 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
+// Prettier
 
-  // Если возраст в диапазоне 11-19, то "лет"
-  // Если не пред. и возраст кончается на 1, то это "год"
-  // Если не пред. и кончается на 2-4, то "года"
-  // Если не пред. и кончается на 5-9 или 0, то "лет"
-  function getAgeWord(age) {
-    const lastAgeDigit = age % 10;
-    const lastTwoDigits = age % 100;
-
-    if (lastTwoDigits > 10 && lastTwoDigits < 20) return "лет";
-
-    if (lastAgeDigit === 1) return "год"
-    if (lastAgeDigit > 1 && lastAgeDigit < 5) return "года"
-    
-    return "лет"
-  }
+const styles = {
+  display: 'flex',
+  flexDirection: "column",
+  width: "fit-content",
+  margin: "2rem auto",
+  padding: '24px',
+  border: '1px solid black'
+}
 
 function App() {
-  const [state, setState] = useState({
-    age: 20,
-    name: "Джон",
-  })
+  const name = "Кеды";
+  const [cost, setCost] = useState(1500);
   
+  let priceSpan;
 
-  let ageWord = getAgeWord(state.age);
-
-  function updateAge(newAge) {
-      setState({
-        age: newAge,
-        name: state.name
-      })
-      // state.age = newAge // не можем!
+  if (cost > 0) {
+    priceSpan = (<span>{cost} ₽</span>)
+  } else {
+    priceSpan = (<span>Бесплатновое</span>)
   }
 
-  const [age, setAge] = useState(state.age)
-
-
-  function handleInput(event) {
-    setAge(+event.target.value)
-  }
-
-  function handleClick(event) {
-    updateAge(age);
-  }
-
-  const wrapperStyles = {
-    padding: "16px",
-    border: "1px solid black",
-    borderRadius: "12px",
-    "background-color":"#bababa"
+  function makeFree() {
+    setCost(0);
   }
 
   return (
     <>
-      <div style={wrapperStyles}>
-        <h3 className="title-text">{ state.name } ({ state.age } {ageWord})</h3>
-          {  state.age > 18 ? (<p>Пользователь совершеннолетний</p>) : "" }
-          <input value={age} onInput={handleInput}></input>
-        <button onClick={handleClick}>Update age</button>
+      <div style={styles}>
+        <h2>{name}</h2>
+        <p>{priceSpan}</p>
+        <p>{ cost > 0 ? `${cost} ₽`: "Бесплатновое" }</p>
+        { cost > 0 &&
+          (<button onClick={makeFree}>
+            Сделать коммунизм
+          </button>)
+        }
       </div>
-      {/* <form style={{
-        padding: "16px",
-        border: "1px solid black",
-        backgroundColor: "#8e9674"
-      }}>
-        <div>
-          <label style={labelStyles}>E-mail: </label>
-          <input style={inputStyles}></input>
-        </div>
-        <div>
-          <label style={labelStyles}>Password: </label>
-          <input style={inputStyles}></input>
-        </div>
-        <button className="submit-btn">Submit</button>
-      </form> */}
     </>
   )
 }
 
 export default App
-
-// Практика 1: Добавить форму с 2 полями: почта и пароль + кнопка
-//  Стилизовать их: форме добавить отступы (padding), 
-//    границу и цвет фона (через стили)
-//    
-//  Для полей добавить ширину на всю форму (через стили в переменной)
-//  Для кнопки: цвет фона, скругление и подсветку при наведении
-//    (через классы)
-
-
-
-
-// Практика 1:
-//   Карточку в корзине товаров, в ней должны быть поля
-//   - название товара
-//   - стоимость товара
-//   - кол-во товаров для добавления (числовой инпут)
-
-// Практика 1.2
-//   Добавьте в карточку:
-//   - поле текущее количество (не инпут)
-//   - кнопку "Добавить"
-//   При нажатии на кнопку число в инпуте добавляется к
-//   текущему количеству
-
-// Практика 2:
-// Создать радио-переключатель, который
-//  меняя сотояние изменяет подпись "вкл" / "выкл"
-// Используйте обработчик onChange
