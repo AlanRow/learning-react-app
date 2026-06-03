fe49737cc667a3d4d29f574eba44c5abb93c48e1
Student <Student@top.ru>
2026-06-03 21:16:24 +0300

    add react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,9 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 
-import CatImage1 from "./assets/cat.jpg"
-import CatImage2 from "./assets/cat2.jpg"
-
-const IMAGES_LIST = [CatImage1, CatImage2]
-
 function App() {
-  const [ imageIndex, setImageIndex ] = useState(0)
-
-  function nextImage() {
-    setImageIndex((imageIndex + 1) % IMAGES_LIST.length);
-  }
-
-  function prevImage() {
-    setImageIndex(imageIndex <= 0 ? 
-      IMAGES_LIST.length - 1 : imageIndex - 1);
-  }
-
-  const image = IMAGES_LIST[imageIndex];
-
   return (
     <>
-    <img width="100" height="100" src={image} />
-    <div>
-      <button onClick={prevImage}>Prev</button>
-      <button onClick={nextImage}>Next</button>
-    </div>
     </>
   )
 }
