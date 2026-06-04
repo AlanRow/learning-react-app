d83b5639337ae318d18bb00b21d388274e610835
AlanRow <alekroga@gmail.com>
2026-06-04 14:10:24 +0300

    break the preview mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,54 @@
 import { useState, useEffect } from 'react'
 import './App.css'
+import { v4 } from "uuid"
 
 function App() {
+  const [ shopCards, setShopCards ]  = useState([
+    {
+      id: "10",
+      title: "Миксер",
+      price: 120,
+    },
+    {
+      id: "20",
+      title: "Телевизор",
+      price: 300,
+    },
+    {
+      id: "30",
+      title: "Флешка",
+      price: 50,
+    },
+    {
+      id: "40",
+      title: "Ручка",
+      price: 20,
+    }
+  ])
+
+  function addNewCard() {
+    setShopCards([
+      ...shopCards,
+      {
+        id: v4(),
+        title: "Новая карточка",
+        price: 0,
+      }
+    ])
+  }
+
   return (
     <>
+      <div className='container'>
+        { shopCards.map((card) => (<div className='card'>
+          <h3>{ card.title }</h3>
+          <p>Цена: { card.price }</p>
+        </div>))
+        }
+      </div>
+      <div>
+        <button onClick={addNewCard}>Добавить</button>
+      </div>
     </>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import pkg from './package.json' assert { type: 'json' };
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: Object.keys(pkg.devDependencies || {}),
+    },
+  },
 })
