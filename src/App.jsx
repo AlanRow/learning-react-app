1e822eade572d60aa1d80c776d1821f67286b3b8
Student <Student@top.ru>
2026-06-08 19:22:41 +0300

    Add simple routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import AuthForm from './components/AuthForm'
+
 function App() {
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+            <Route path="/" element={ 
+              <div>
+                <h1> Welcome! </h1>
+                <Link to="/auth">Login</Link>
+              </div>
+            } />
+            <Route path="/auth" element={ <AuthForm /> }/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
@@ -11,8 +25,12 @@ function App() {
 export default App
 
 // Практика 1:
-//  У нас есть сайт со списком картинок из которых 
-//  показана лишь одна, все картинки лежат в списке
-//  и текущую для показа надо переключать кнопками 
-//  "Предыдущая" и "Следующая" 
+//  Надо написать страницу сайта цветочного магазина
+//  (или любой другой сайт) с хедером, в котором
+//  находятся две вкладки:
+//  1) основная страница с описанием сайта (/)
+//  2) страница с контактами (телефон и почта) (/contacts)
+// 
+//  Для ссылок в хедере используйте Link
+
 
---
+++ src/components/AuthForm.jsx
@@ -0,0 +1,8 @@
+function AuthForm() {
+    return (<form>
+       <div><label>Login: </label><input /></div> 
+        <div><label>Password: </label><input type="password" /></div>
+    </form>)
+}
+
+export default AuthForm
