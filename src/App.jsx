2d9a2cb457844e5dbb683c27c372ca9c98dd149c
Student <Student@top.ru>
2026-06-15 20:37:47 +0300

    useRef for HTML elements practice

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,17 @@ import Stopwatch from "./components/Stopwatch"
 import ToggleButton from './components/ToggleButton';
 
 function App() {
+  const inputRef = useRef(null)
+
+  function focusInput() {
+    inputRef.current.focus()
+  }
+
   return (
     <>
     <div>
-      <ToggleButton />
+      <label onClick={focusInput}>Input: </label>
+      <input ref={inputRef}></input>
     </div>
     </>
   )
@@ -21,6 +28,16 @@ export default App
 // Состояние меняется при каждом клике
 // Помимо этого при каждом клике в консоль должно выводиться
 //  Текущее число кликов (используйте useRef)
+
+// Практика 2:
+//  Напишите программу, которая рисует прямоугольник
+//  с высотой 50% и шириной 50% экрана (через CSS)
+//  И добавбье кнопку при клике на которую
+//  В алерте будет выводиться ширина и высота
+//  прямоугольника в пикселях
+
+// Используйте любые свойства ширины и высоты
+// (e. g. offsetWidth / offsetHeight)
 
 
 
