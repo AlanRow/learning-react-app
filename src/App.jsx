bbbbd4409a842942b8eef8901f28f5177b2b9711
Student <Student@top.ru>
2026-06-15 20:23:36 +0300

    useRef simple practice implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,14 @@
 import { useState, useEffect, useRef } from 'react'
 import './App.css'
 import Stopwatch from "./components/Stopwatch"
-
-
+import ToggleButton from './components/ToggleButton';
 
 function App() {
-  let counter1 = 0;
-
-  function upCounter1() {
-    counter1 += 1;
-  }
-
-  const [ counter2, setCounter2 ] = useState(0)
-  function upCounter2() {
-    setCounter2(counter2 + 1)
-  }
-
-  let counter3 = useRef(0)
-  function upCounter3() {
-    counter3.current += 1
-  }
-
   return (
     <>
-      <div>
-        <p>Counter 1: {counter1}</p>
-        <button onClick={upCounter1}>Up</button>
-      </div>
-      <div>
-        <p>Counter 2: {counter2}</p>
-        <button onClick={upCounter2}>Up</button>
-      </div>
-      <div>
-        <p>Counter 3: {counter3.current}</p>
-        <button onClick={upCounter3}>Up</button>
-      </div>
+    <div>
+      <ToggleButton />
+    </div>
     </>
   )
 }
