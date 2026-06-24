d1c41fa529dcb260547922d6fa93f3edc8fd57f9
Student <Student@top.ru>
2026-06-24 20:40:03 +0300

    useMemo

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ const MemoizedList = memo(List)
 function App() {
   const [ toggle, setToggle ] = useState(true)
   const [length, setLength] = useState(1)
+  const [delimiter, setDelimiter] = useState(", ")
 
   return (
     <>
@@ -14,13 +15,17 @@ function App() {
       <button onClick={() => setToggle(!toggle)}>
         { toggle ? "Вкл." : "Выкл." }
       </button>
-      <MemoizedList length={length} />
+      <MemoizedList length={length} delimiter={delimiter} />
       <button onClick={() => setLength(length - 1)}>
         -
       </button>
       <button onClick={() => setLength(length + 1)}>
         +
       </button>
+      <input
+        value={delimiter}
+        onChange={(e) => setDelimiter(e.target.value)}
+      />
     </div>
     </>
   )
@@ -36,6 +41,19 @@ export default App
 //  Добавьте в компонент Button console.log
 //  или alert, который будем вызываться при каждом
 //  рендере
+
+// Практика 2:
+// Напишите компонент MultipliedResult, который
+//  принимает три пропса: a, b и multyplyCharacter
+//  Он должен выводить на экран строку вида:
+//  "{a} {multiplyCharacter} {b} = {a * b}",
+//  Например, при a = 2, b = 3 и 
+//   multiplyCharacter = "X", Должно вывести строку
+//  "2 X 3 = 6"
+
+//  Добавьте компонент в App.jsx и привяжите a и b 
+// к input'ам, a multiplyCharacter к селекту с двумя
+//  вариантами: X и *
 
 
 
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,13 +1,31 @@
-function List({ length }) {
+import { useMemo } from "react";
+
+function getFactorial(n) {
+    if (n === 1) return 1;
+
+    return getFactorial( n - 1) * n;
+}
+
+function getFactorialsList(n) {
+    console.log("List calculating...")
+
+    return [
+        ...new Array(n)
+    ].map((_, i) => getFactorial(i + 1))
+}
+
+function List(props) {
     console.log("List rendered")
 
-    const list = [
-        ...new Array(length)
-    ].map((_, i) => i+1)
+    // const list = getFactorialsList(props.length)
 
-    return (<ul>
-        {list.map(n => <li>{n}</li>)}
-    </ul>)
+    const list = useMemo(
+        () => getFactorialsList(props.length),
+        [props.length]
+    )
+    const listLine = list.join(props.delimiter)
+
+    return (<div>{ listLine }</div>)
 }
 
 export default List;
