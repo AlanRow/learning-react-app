8787526264fca752ea6575aa79a38f3033709d7a
Student <Student@top.ru>
2026-06-29 20:17:36 +0300

    useMemo for objects / arrays

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,35 @@ function sum(x, y) {
   return x + y
 }
 
+function sumMultiple(sumArr) {
+  console.log("Summarize...")
+  return sumArr.reduce((prev, cur) => prev + cur, 0)
+}
+
+// Option 1
+// const SUM_VARS = [1, 1]
+
 function App() {
   const [ toggle, setToggle ] = useState(true)
 
-  const [a, setA] = useState(1)
-  const [b, setB] = useState(1) 
+  // const [a, setA] = useState(1)
+  // const [b, setB] = useState(1)
 
-  const abSum = useMemo(
-    () => sum(a, b),
-    [a, b]
-  )
+  // const abSum = useMemo(
+  //   () => sum(a, b),
+  //   [sumVars]
+  // )
+
+  // Option 2
+  // const sumVars = useMemo(() => [a, b], [a, b])
+
+  // Option 3
+  const [sumVars, setSumVars] = useState([1, 1])
+  const sumOfVars = useMemo(() => sumMultiple(sumVars), sumVars)
+
+  function setArg(value, index) {
+    setSumVars(sumVars.map((n, i) => i === index ? value : n))
+  }
 
   return (
     <>
@@ -25,12 +44,19 @@ function App() {
         { toggle ? "Вкл." : "Выкл." }
       </button>
 
-      <div>
+      {/* <div>
         <input value={a} onChange={(e) => setA(Number(e.target.value))} />
         +
         <input value={b} onChange={(e) => setB(Number(e.target.value))} />
         =
-        <span>{ abSum }</span>
+        <span>{ sum }</span>
+      </div> */}
+      <div>
+        <input value={sumOfVars[0]} onChange={(e) => setArg(Number(e.target.value), 0)} />
+        +
+        <input value={sumOfVars[1]} onChange={(e) => setArg(Number(e.target.value), 1)} />
+        =
+        <span>{ sumOfVars }</span>
       </div>
     </div>
     </>
