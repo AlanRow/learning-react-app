8aa109af7194f90d5d580f98ff66d7b1981a78f0
Student <Student@top.ru>
2026-06-29 19:25:01 +0300

    Another useMemo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import { useState, memo } from 'react'
+import { useState, useMemo } from 'react'
 import './App.css'
-import List from './components/List'
 
-const MemoizedList = memo(List)
+
+function sum(x, y) {
+  console.log("Summarize...")
+  return x + y
+}
 
 function App() {
   const [ toggle, setToggle ] = useState(true)
-  const [length, setLength] = useState(1)
-  const [delimiter, setDelimiter] = useState(", ")
+
+  const [a, setA] = useState(1)
+  const [b, setB] = useState(1) 
+
+  const abSum = useMemo(
+    () => sum(a, b),
+    [a, b]
+  )
 
   return (
     <>
@@ -15,45 +24,20 @@ function App() {
       <button onClick={() => setToggle(!toggle)}>
         { toggle ? "Вкл." : "Выкл." }
       </button>
-      <MemoizedList length={length} delimiter={delimiter} />
-      <button onClick={() => setLength(length - 1)}>
-        -
-      </button>
-      <button onClick={() => setLength(length + 1)}>
+
+      <div>
+        <input value={a} onChange={(e) => setA(Number(e.target.value))} />
         +
-      </button>
-      <input
-        value={delimiter}
-        onChange={(e) => setDelimiter(e.target.value)}
-      />
+        <input value={b} onChange={(e) => setB(Number(e.target.value))} />
+        =
+        <span>{ abSum }</span>
+      </div>
     </div>
     </>
   )
 }
 
 export default App
-
-// Практика 1:
-//  Добавьте компонент Button, с параметром text 
-//  в его props
-//  Вставьте в App.jsx мемоизированный компонент 
-//  MemoizedButton
-//  Добавьте в компонент Button console.log
-//  или alert, который будем вызываться при каждом
-//  рендере
-
-// Практика 2:
-// Напишите компонент MultipliedResult, который
-//  принимает три пропса: a, b и multyplyCharacter
-//  Он должен выводить на экран строку вида:
-//  "{a} {multiplyCharacter} {b} = {a * b}",
-//  Например, при a = 2, b = 3 и 
-//   multiplyCharacter = "X", Должно вывести строку
-//  "2 X 3 = 6"
-
-//  Добавьте компонент в App.jsx и привяжите a и b 
-// к input'ам, a multiplyCharacter к селекту с двумя
-//  вариантами: X и *
 
 
 
